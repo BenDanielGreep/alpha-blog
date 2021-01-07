3c4ffe39a57fa71c3378afce7f176e46c9a71ac9
Ben DG <bendanielgreep@gmail.com>
2021-01-07 14:40:41 +0000

    add user signup form, add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ root 'pages#home'
 get 'about', to: 'pages#about'
 # get 'edit', to: 'pages#edit' #i attempted this line
 resources :articles
-
+get 'signup', to: 'users#new'
+post 'users', to: 'users#create'
+resources :users, except: [:new]
 
 end
