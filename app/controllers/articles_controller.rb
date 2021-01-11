3a87837e63ddd1b2d9ada50724fe217d3624c2f3
Ben Daniel-Greep <bendanielgreep@gmail.com>
2021-01-11 15:13:41 +0000

    restrict actions at controller level for articles and users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,20 @@
 class ArticlesController < ApplicationController
 	before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
 	def show
 	end
 
 	def index
 		@articles = Article.paginate(page: params[:page], per_page: 5)
-	end	
-
-	def new
-		@article = Article.new	
 	end
 
-	def edit 
+	def new
+		@article = Article.new
+	end
+
+	def edit
 	end
 
 	def create
@@ -21,8 +23,8 @@ class ArticlesController < ApplicationController
 		 if @article.save
 		 	flash[:notice] = "Article was created successfully"
 			redirect_to @article
-		else 
-		  	render 'new'	
+		else
+		  	render 'new'
 		 end
 	end
 
@@ -30,14 +32,14 @@ class ArticlesController < ApplicationController
 		if @article.update(article_params)
 			flash[:notice] = "Article was updated successfully"
 			redirect_to @article
-		else 
+		else
 			render 'edit'
 		end
 	end
 
 	def destroy
 		@article.destroy
-		redirect_to articles_path			
+		redirect_to articles_path
 	end
 
 	private
@@ -47,7 +49,14 @@ class ArticlesController < ApplicationController
 	end
 
 	def article_params
-		params.require(:article).permit(:title, :description)	
+		params.require(:article).permit(:title, :description)
 	end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
+    end
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
  before_action :set_user, only: [:show, :edit, :update]
+ before_action :require_user, only: [:edit, :update]
+ before_action :require_same_user, only: [:edit, :update, :destroy]
 
 	def show
-			
+
 			@articles = @user.articles.paginate(page: params[:page], per_page: 5)
 	end
 
@@ -16,15 +18,15 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		
+
 	end
 
 	def update
-		
+
 		if @user.update(user_params)
 	   flash[:notice] = "Account successfully updated"
 		redirect_to @user
-		else	
+		else
 			render 'edit'
 		end
 
@@ -42,12 +44,20 @@ class UsersController < ApplicationController
 	end
 
 	private
+
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
 	end
 
 	def set_user
-@user = User.find(params[:id])
+  @user = User.find(params[:id])
 	end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit or delete your own account"
+      redirect_to @user
+    end
+  end
 
 end
