3242dbd018d86874529486b471487d8ac8e2ec7a
Ben Daniel-Greep <bendanielgreep@gmail.com>
2021-01-11 17:20:43 +0000

    add delete user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
 	before_save { self.email = email.downcase}
-	has_many :articles
-	validates :username, presence: true, 
-											uniqueness: { case_sensitive: false }, 
+	has_many :articles, dependent: :destroy
+	validates :username, presence: true,
+											uniqueness: { case_sensitive: false },
 											length: { minimum: 3, maxmimum: 25 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, 
-										uniqueness: { case_sensitive: false }, 
+	validates :email, presence: true,
+										uniqueness: { case_sensitive: false },
 										length: { minimum: 10, maxmimum: 100 },
 										format: { with: VALID_EMAIL_REGEX }
    has_secure_password
